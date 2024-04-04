a51d9961bc3d3a40ec81090ea6ea9a10c40345e9
eharnlee <eharnlee@gmail.com>
2024-04-04 13:12:55 +0800

    Update functions to use variables from function argument

--- pre_process.py
+++ pre_process.py
@@ -1,32 +1,23 @@
 from keras.preprocessing.image import ImageDataGenerator
 
 
-def pre_process():
-    # Define data augmentation and preprocessing
-    datagen = ImageDataGenerator(
-        rescale=1.0 / 255,
-        rotation_range=20,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        validation_split=0.2,  # Split data into training and validation
-    )
+def pre_process(train_data_path: str, data_generator: ImageDataGenerator):
 
     # Load and preprocess data
-    train_generator = datagen.flow_from_directory(
-        "data/train",
+    train_generator = data_generator.flow_from_directory(
+        train_data_path,
         target_size=(224, 224),
         batch_size=32,
         class_mode="binary",
         subset="training",
     )
 
-    validation_generator = datagen.flow_from_directory(
-        "data/train",
+    validation_generator = data_generator.flow_from_directory(
+        train_data_path,
         target_size=(224, 224),
         batch_size=32,
         class_mode="binary",
         subset="validation",
     )
+
+    return train_generator, validation_generator
--- train.py
+++ train.py
@@ -1,4 +1,12 @@
-def train(model):
+from keras.models import Model
+from keras.preprocessing.image import DirectoryIterator
+
+
+def train(
+    model: Model,
+    train_generator: DirectoryIterator,
+    validation_generator: DirectoryIterator,
+):
     history = model.fit(
         train_generator,
         steps_per_epoch=len(train_generator),
