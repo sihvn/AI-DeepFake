c8ccc0f62e5cc68d5af6252a9143d649feb368e9
eharnlee <eharnlee@gmail.com>
2024-04-08 13:27:39 +0800

    Update process_frame.extract_faces_from_frames to ignore faces smaller than 75x75 px
    helps to ignore wrong faces from background objects

--- process_frame.py
+++ process_frame.py
@@ -1,28 +1,35 @@
 import os
+
 import face_recognition
 from PIL import Image
+
 
 def extract_faces_from_frames(input_folder, output_folder):
     # Create the output folder if it doesn't exist
     os.makedirs(output_folder, exist_ok=True)
-    
+
     for filename in os.listdir(input_folder):
-        if filename.endswith(('.jpg', '.jpeg', '.png')):
+        if filename.endswith((".jpg", ".jpeg", ".png")):
             image_path = os.path.join(input_folder, filename)
             image = face_recognition.load_image_file(image_path)
-            
+
             # Find all face locations in the image
             face_locations = face_recognition.face_locations(image)
-            
+
             # Extract and save faces
             for i, (top, right, bottom, left) in enumerate(face_locations):
-                face_roi = image[top:bottom, left:right]
-                face_path = os.path.join(output_folder, f"{os.path.splitext(filename)[0]}_face_{i}.jpg")
-                face_image = Image.fromarray(face_roi)
-                face_image.save(face_path)
-                print(f"Saved face {i} from {filename}")
+                # only extract faces greater than 75x75 pixels --> helps to ignore wrong faces from background objects
+                if (bottom - top > 75) and (right - left > 75):
+                    face_roi = image[top:bottom, left:right]
+                    face_path = os.path.join(
+                        output_folder, f"{os.path.splitext(filename)[0]}_face_{i}.jpg"
+                    )
+                    face_image = Image.fromarray(face_roi)
+                    face_image.save(face_path)
+                    print(f"Saved face {i} from {filename}")
 
-input_folder = "dataset/frames"
-output_folder = "train/fake"
+
+input_folder = "dataset\\DeepFakeDetection1\\real-frames"
+output_folder = "dataset\\DeepFakeDetection1-faces\\real"
 
 extract_faces_from_frames(input_folder, output_folder)
