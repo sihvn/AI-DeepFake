70cdbf2ef2fbca6aa5181cc6e49fdf868759eb3e
eharnlee <eharnlee@gmail.com>
2024-04-08 13:50:07 +0800

    Update temp model

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ from pre_process import *
 from train import *
 
 
-def main(train_data_path: str, test_data_path: str):
+def main(
+    train_data_path: str,
+    test_data_path: str,
+    weights_output_path: str,
+    weights_input_path: str = "",
+):
     print("\nAI Project - DeepFake Detection\n")
 
     # # Define data augmentation and preprocessing
@@ -38,9 +43,15 @@ def main(train_data_path: str, test_data_path: str):
     # print("Evaluating model on test data...")
     # evaluate(model, test_data_path, data_generator)
 
+    # ----------------------------------------------------------------------------------------------------
+    # Temporary section below for testing and debugging. Restore section above after model is working.
+    # ----------------------------------------------------------------------------------------------------
+
+    # Source: https://jasminbharadiya.medium.com/mastering-deepfake-detection-unleashing-the-power-of-resnet-for-absolute-accuracy-94754376d0a1
+
     # Define data augmentation and preprocessing
     datagen = ImageDataGenerator(
-        # rescale=1.0 / 255,
+        rescale=1.0 / 255,
         rotation_range=20,
         width_shift_range=0.2,
         height_shift_range=0.2,
@@ -54,7 +65,7 @@ def main(train_data_path: str, test_data_path: str):
     train_generator = datagen.flow_from_directory(
         train_data_path,
         target_size=(224, 224),
-        batch_size=10,
+        batch_size=32,
         class_mode="binary",
         subset="training",
     )
@@ -62,24 +73,19 @@ def main(train_data_path: str, test_data_path: str):
     validation_generator = datagen.flow_from_directory(
         train_data_path,
         target_size=(224, 224),
-        batch_size=10,
+        batch_size=32,
         class_mode="binary",
         subset="validation",
     )
 
     test_generator = datagen.flow_from_directory(
-        test_data_path, target_size=(224, 224), batch_size=10, class_mode="binary"
+        test_data_path, target_size=(224, 224), batch_size=32, class_mode="binary"
     )
 
-    # Load pre-trained EfficientNetB0
-    base_model = EfficientNetB0(
+    # Load pre-trained ResNet50
+    base_model = ResNet50(
         weights="imagenet", include_top=False, input_shape=(224, 224, 3)
     )
-
-    # # Load pre-trained ResNet50
-    # base_model = ResNet50(
-    #     weights="imagenet", include_top=False, input_shape=(224, 224, 3)
-    # )
 
     # Add custom classification head
     x = base_model.output
@@ -96,6 +102,7 @@ def main(train_data_path: str, test_data_path: str):
     # Compile the model
     model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 
+    # Train the model
     history = model.fit(
         train_generator,
         steps_per_epoch=len(train_generator),
@@ -104,20 +111,27 @@ def main(train_data_path: str, test_data_path: str):
         validation_steps=len(validation_generator),
     )
 
-    # loss, accuracy = model.evaluate(test_generator)
-    # print(f"Test loss: {loss:.4f}")
-    # print(f"Test accuracy: {accuracy*100:.2f}%")
+    # Save the weights
+    model.save_weights(weights_output_path)
 
-    evaluation = model.evaluate(test_generator)
-    print(evaluation)
+    loss, accuracy = model.evaluate(test_generator)
+    print(f"Test loss: {loss:.4f}")
+    print(f"Test accuracy: {accuracy * 100:.2f}%")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Arguments missing.")
-        print("Usage: main.py [train_data_path] [test_data_path]")
+    # if len(sys.argv) < 4:
+    #     print("Arguments missing.")
+    #     print("Usage: main.py <train_data_path> <test_data_path> <weights_output_path> [<weights_input_path>]")
 
-    train_data_path = sys.argv[1]
-    test_data_path = sys.argv[2]
+    # else:
+    #     train_data_path = sys.argv[1]
+    #     test_data_path = sys.argv[2]
 
-    main(train_data_path, test_data_path)
+    #     main(train_data_path, test_data_path)
+    train_data_path = "dataset/train-faces"
+    test_data_path = "dataset/test-faces"
+    weights_output_path = "weights/trial_weights_1"
+    weights_input_path = ""
+
+    main(train_data_path, test_data_path, weights_output_path)
